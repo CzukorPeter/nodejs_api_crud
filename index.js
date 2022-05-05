f77bc9dcee94700c699a4cb041225243080dbbe8
CzukorPeter <peterczukor@gmail.com>
2022-05-05 06:13:33 +0200

    first endpoint for test and nodemon

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const server = express();
 
 const PORT = 1550;
 
+server.get('/', (req, res) => {
+    res.json({ endpoint: " First endpoint for testing."})
+});
+
+server.get('/nodemon', (req, res) => {
+    res.json({ nodemon: " Nodemon endpoint for testing nodeman tool."})
+});
+
 server.listen(1550, () => {
     console.log(`Server running at port: ${PORT}`);
 })
