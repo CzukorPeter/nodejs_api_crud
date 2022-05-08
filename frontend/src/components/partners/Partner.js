fe114a7211ff59a674b6b0b7bccb9927c5a1d8aa
CzukorPeter <peterczukor@gmail.com>
2022-05-08 16:42:57 +0200

    data display in table format

---
+++ frontend/src/components/partners/Partner.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Partner = ({ partner }) => {
+    return (  
+        <tbody>
+          <tr>
+            <td>{partner.id}</td>
+            <td><a href="#edit-partner-modal" className="red-text"><b>{partner.name}</b></a></td>
+            <td>{partner.companyform}</td>
+            <td>{partner.tax_number}</td>
+            <td>{partner.company_reg_number}</td>
+            <td>{partner.settlements}</td>
+            <td>{partner.address}</td>
+            <td>{partner.phone_number}</td>
+            <td>{partner.bank_account_number}</td>
+            <td>{partner.comment}</td>
+            <td>
+            <a href="!#" className="red-text">
+            <i className="material-icons">delete</i>
+            </a>
+            </td>
+            <td>
+            <a href="!#" className="green-text">
+            <i className="material-icons">edit</i>
+            </a>
+            </td>
+          </tr>
+        </tbody>
+
+    )
+}
+
+Partner.propTypes = {
+    partner: PropTypes.object.isRequired,
+}
+
+export default Partner
--- frontend/src/components/partners/Partners.js
+++ frontend/src/components/partners/Partners.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Partner  from "./Partner";
 
 const Partners = () => {
     const [partners, setPartners] = useState([]);
@@ -23,15 +24,29 @@ const Partners = () => {
     }
 
     return (
-    <ul className="collection with-header">
-        <li className="collection-header">
-            <h4 className="center">Partners</h4>
-        </li>
+    <div className="">
+        <h4 className="center">Partners</h4>
+        <table className="centered">
+        <thead>
+          <tr>
+              <th>#Id</th>
+              <th>Name</th>
+              <th>Company form</th>
+              <th>Tax number</th>
+              <th>Company registration number</th>
+              <th>Settlement</th>
+              <th>Adress</th>
+              <th>Phone number</th>
+              <th>Bank account number</th>
+              <th>Comment</th>
+          </tr>
+        </thead>
         {! loading && partners.length === 0 ? (<p className="cente">No partners in the database...</p>) : (
-            partners.map(partner => <li>{partner.name}</li>)
+            partners.map(partner => <Partner partner={partner} key={partner.id} />)
         )}
+              </table>
 
-    </ul>
+    </div>
     )
 }
 
